e8df143a3d46cdb18a8ad3156ff809e53f28e27f
Sumon Nayek <34118864+sumonnayek@users.noreply.github.com>
2020-05-04 15:58:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AutoComplete from "./components/AutoComplete";
 
 function App() {
+  let countries = [
+    "India",
+    "England",
+    "Australia",
+    "US",
+    "SA",
+    "Canada",
+    "Indonasia",
+    "China",
+    "Ireland",
+    "Iran"
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AutoComplete
+        countries={countries}
+      />
     </div>
   );
 }
---
+++ src/components/AutoComplete.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+export class AutoComplete extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filteredCountries: [],
+      showCountry: false,
+      enteredValue: ""
+    };
+  }
+
+  onChange = e => {
+    console.log("onChange");
+    const enteredValue = e.currentTarget.value;
+    console.log(enteredValue);
+
+    const filteredCountries = this.props.countries.filter(
+      country =>
+        country.toLowerCase().indexOf(enteredValue.toLowerCase()) !== -1
+    );
+
+    this.setState({
+      filteredCountries,
+      enteredValue: e.currentTarget.value,
+      showCountry: true
+    });
+  };
+
+  render() {
+    const { filteredCountries } = this.state;
+    let countryList;
+
+    if (filteredCountries) {
+      countryList = (
+        <ul className="listContainer">
+          {filteredCountries.map(country => {
+            return <li key={country}>{country}</li>;
+          })}
+        </ul>
+      );
+    }
+
+    return (
+      <>
+        <div className="search-container">
+          <input onChange={this.onChange} className="search-box1" type="text" />
+        </div>
+        {countryList}
+      </>
+    );
+  }
+}
+
+export default AutoComplete;
